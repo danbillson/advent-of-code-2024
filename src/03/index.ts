efa8ffb8d2a794885471dfac1e101d2ef606cab6
Dan Billson <dbillson@outlook.com>
2024-12-03 07:37:17 +0000

    🙂‍↔️ day 3 - 2

--- src/03/index.ts
+++ src/03/index.ts
@@ -5,6 +5,9 @@ const data = await file.text();
 const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
 const matches = data.match(regex) as string[];
 
+const fullRegex = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g;
+const doDontMatches = data.match(fullRegex) as string[];
+
 console.log("Part 1:", part1());
 console.log("Part 2:", part2());
 
@@ -16,5 +19,21 @@ function part1() {
 }
 
 function part2() {
-  return null;
+  let match = true;
+  return doDontMatches.reduce((acc, cur) => {
+    if (cur === "do()") {
+      match = true;
+      return acc;
+    }
+
+    if (cur === "don't()") {
+      match = false;
+      return acc;
+    }
+
+    if (!match) return acc;
+
+    const [_, a, b] = cur.match(/mul\((\d{1,3}),(\d{1,3})\)/) as string[];
+    return acc + Number(a) * Number(b);
+  }, 0);
 }
