1fdd999c1acb2ab33c2dce9c835016570d9390d0
Dan Billson <dbillson@outlook.com>
2024-12-09 20:12:09 +0000

    😢 day 9 - 2

--- src/09/index.ts
+++ src/09/index.ts
@@ -51,6 +51,70 @@ function part1() {
   return checksum(sorted);
 }
 
+type File = { id: number; size: number; gap: number };
+
+function formatFiles(data: string) {
+  return data.split("").reduce((acc, cur, i) => {
+    if (i % 2 !== 0) return acc;
+
+    return [
+      ...acc,
+      {
+        id: Math.floor(Number(i) / 2),
+        size: Number(cur),
+        gap: Number(data[i + 1]) || 0,
+      },
+    ];
+  }, [] as File[]);
+}
+
+function sortFiles(data: File[]) {
+  let sorted = [...data];
+  let seen = new Set<number>();
+
+  for (let i = sorted.length - 1; i > 0; i--) {
+    const { id, size, gap: gapAhead } = sorted[i];
+    if (seen.has(id)) continue;
+    seen.add(id);
+
+    for (let j = 0; j < i; j++) {
+      const { gap } = sorted[j];
+      if (gap >= size) {
+        // Adjust gaps
+        sorted[j].gap = 0;
+        sorted[i].gap = gap - size;
+        sorted[i - 1].gap += size + gapAhead;
+
+        // Move file
+        sorted.splice(j + 1, 0, sorted[i]);
+        sorted.splice(i + 1, 1);
+        i++;
+        break;
+      }
+    }
+  }
+
+  return sorted;
+}
+
+function checksumFiles(data: File[]) {
+  let i = 0;
+
+  return data.reduce((acc, cur) => {
+    let sum = 0;
+    for (let j = 0; j < cur.size; j++) {
+      sum += cur.id * i;
+      i++;
+    }
+    i += cur.gap;
+
+    return acc + sum;
+  }, 0);
+}
+
 function part2() {
-  return null;
+  const formatted = formatFiles(data);
+  const sorted = sortFiles(formatted);
+
+  return checksumFiles(sorted);
 }
