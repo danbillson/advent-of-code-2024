43f10061bde97099dd7ded4517d413d745003ce8
Dan Billson <dbillson@outlook.com>
2024-12-13 12:56:08 +0000

    💸 day 13 - 2

--- src/13/index.ts
+++ src/13/index.ts
@@ -2,19 +2,23 @@ const path = "./src/13/data.txt";
 const file = Bun.file(path);
 
 const data = await file.text();
-const machines = data.split("\n\n").map((group) => {
-  const [a, b, prize] = group
-    .split("\n")
-    .map((line) => line.match(/\d+/g)?.map(Number) as [number, number]);
+const groups = data.split("\n\n");
 
-  return {
-    a: { x: a[0], y: a[1], cost: 3 },
-    b: { x: b[0], y: b[1], cost: 1 },
-    prize: { x: prize[0], y: prize[1] },
-  };
-});
+function formatMachines(groups: string[], offset = 0) {
+  return groups.map((group) => {
+    const [a, b, prize] = group
+      .split("\n")
+      .map((line) => line.match(/\d+/g)?.map(Number) as [number, number]);
 
-type Machine = (typeof machines)[number];
+    return {
+      a: { x: a[0], y: a[1], cost: 3 },
+      b: { x: b[0], y: b[1], cost: 1 },
+      prize: { x: prize[0] + offset, y: prize[1] + offset },
+    };
+  });
+}
+
+type Machine = ReturnType<typeof formatMachines>[0];
 
 // Cramers rule - https://www.cuemath.com/algebra/cramers-rule/
 function cost({ a, b, prize }: Machine) {
@@ -37,11 +41,15 @@ function cost({ a, b, prize }: Machine) {
 }
 
 function part1() {
+  const machines = formatMachines(groups);
+
   return machines.reduce((acc, machine) => acc + cost(machine), 0);
 }
 
 function part2() {
-  return null;
+  const machines = formatMachines(groups, 10000000000000);
+
+  return machines.reduce((acc, machine) => acc + cost(machine), 0);
 }
 
 console.log("Part 1:", part1());
